8562cfe676ca486d7c9f208aa2c459052d0a6fba
manman <maria_1313@163.com>
2017-02-17 22:59:03 +0800

    测试强制修改用户名和信息。

--- vue2/src/components/Modules/Demo/User/Edit/Edit.js
+++ vue2/src/components/Modules/Demo/User/Edit/Edit.js
@@ -50,6 +50,13 @@ module.exports = {
 
                     // console.log(this[userdata].status);
 
+                    //测试：强制修改用户名或者状态时，接口返回不允许修改的错误信息
+                    /*if (this[userdata].id) {
+                        // this[userdata].username='testupdatename';
+
+                        // this[userdata].status=!this[userdata].status;
+                    }*/
+
                     UserApi.saveUser.call(this, this[userdata], data => {
                         this.$router.push('/demo/user/list');
                     });
