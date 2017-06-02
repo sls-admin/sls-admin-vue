9652faa8acb907b5af9a4f8ae995abc2087628d4
sailengsi <sailengsi@126.com>
2017-06-02 13:13:33 +0800

    修复demo模块编辑文章，富文本编辑器wangeditor采用import引入，wangeditor上传配置，在header中新增token，添加修改文章搞定

--- vue2/src/views/Demo/Article/Edit/Edit.js
+++ vue2/src/views/Demo/Article/Edit/Edit.js
@@ -2,6 +2,8 @@ import {
 	gbs
 } from 'config/';
 
+import 'wangeditor';
+
 export default {
 	name   : 'edit-article',
 	data() {
@@ -50,7 +52,8 @@ export default {
 					// 'location',
 					'insertcode', '|',
 					'undo', 'redo', 'fullscreen'
-				]
+				],
+                obj:null
 			}
 		}
 	},
@@ -69,7 +72,6 @@ export default {
 				if (valid) {
 					// console.log(this.article_data.content);
 					// console.log(this.temp.content);
-
 					// return;
 
 					if (!this.temp.content) {
@@ -77,7 +79,6 @@ export default {
 							this.$message.error('文章内容不能为空！');
 							return;
 						}
-						return;
 					}
 
 					// console.log(this.article_data);
@@ -90,76 +91,90 @@ export default {
 					});
 				}
 			});
-		}
-		,
+		},
+
 		setContent(html, text){
 			this.article_data.content = html;
 			this.temp.content         = text;
-		}
-		,
+		},
+
+        /**
+         * 初始化wangeditor编辑器
+         */
+        initWangeditor(){
+            var self   = this;
+            this.wangEditor.obj = new wangEditor('article');
+
+            this.wangEditor.obj.config.uploadImgFileName = 'article';
+
+            this.wangEditor.obj.config.uploadImgUrl = gbs.host + '/Article/editUpload';
+
+            // 配置自定义参数（举例）
+            this.wangEditor.obj.config.uploadParams = {
+                username: this.$store.state.user.userinfo.username
+            };
+
+            //自定义header，因为服务器验证通过header中的token进行的验证
+            this.wangEditor.obj.config.uploadHeaders = {
+                token : this.$store.state.user.userinfo.token
+            };
+
+            // 自定义load事件
+            this.wangEditor.obj.config.uploadImgFns.onload = (data) => {
+                //注意：这是原生ajax的处理,返回的是原生的JSON串
+                if(typeof data==='string'){
+                    data=JSON.parse(data);
+                }
+                // console.log(data);
+                if (data.status === 200) {
+                    // 上传图片时，已经将图片的名字存在 this.wangEditor.obj.uploadImgOriginalName
+                    var originalName = this.wangEditor.obj.uploadImgOriginalName || '';
+
+                    // 如果 resultText 是图片的url地址，可以这样插入图片：
+                    this.wangEditor.obj.command(null, 'insertHtml', '<img src="' + data.data.fileinfo.getSaveName + '" alt="' + originalName + '" style="max-width:100%;"/>');
+                } else {
+                    //这里可以判断到token无效时，可以退出登录，让用户重新登录
+                    if (data.status === 404) {
+                        this.$message.error('上传错误信息：token无效！');
+                    } else {
+                        this.$message.error('上传错误信息：' + data.msg);
+                    }
+                }
+
+            };
+
+            //自定义错误信息
+            this.wangEditor.obj.config.uploadImgFns.onerror = (xhr) => {
+                this.$message.error('上传错误信息：网络错误！');
+            };
+
+            //自定义工具栏
+            this.wangEditor.obj.config.menus = this.wangEditor.bar;
+
+            //编辑器改变事件时，同步更新文章内容
+            this.wangEditor.obj.onchange = function () {
+                var text = this.$txt.text().replace(/(^\s*)|(\s*$)/g, ""),
+                    html = this.$txt.html();
+
+                /*console.log(text);
+                 console.log(html);*/
+
+                self.setContent(html, text);
+            };
+
+            // this.wangEditor.obj.config.hideLinkImg = true;
+
+            //自定义上传图片错误事件
+            this.wangEditor.obj.create();
+        },
+
 		reset_article(article){
 			this.$refs[article].resetFields();
 		}
 	},
 
 	mounted(){
-		var self   = this;
-		var editor = new wangEditor('article');
-
-		// console.log(editor.config);
-
-		// editor.config.jsFilter = false;
-
-		editor.config.uploadImgFileName = 'article';
-
-		editor.config.uploadImgUrl = gbs.host + '/Article/editUpload';
-
-		// 配置自定义参数（举例）
-		editor.config.uploadParams = {
-			token   : this.$store.state.user.userinfo.token,
-			username: this.$store.state.user.userinfo.username
-		};
-
-		// 自定义load事件
-		editor.config.uploadImgFns.onload = (data) => {
-			if (data.status === 200) {
-				// 上传图片时，已经将图片的名字存在 editor.uploadImgOriginalName
-				var originalName = editor.uploadImgOriginalName || '';
-
-				// 如果 resultText 是图片的url地址，可以这样插入图片：
-				editor.command(null, 'insertHtml', '<img src="' + data.data.fileinfo.getSaveName + '" alt="' + originalName + '" style="max-width:100%;"/>');
-			} else {
-				if (data.status === 404) {
-					this.$message.error('上传错误信息：token无效！');
-				} else {
-					this.$message.error('上传错误信息：' + data.msg);
-				}
-			}
-
-		};
-
-		editor.config.uploadImgFns.onerror = (xhr) => {
-			this.$message.error('上传错误信息：网络错误！');
-		};
-
-		editor.config.menus = this.wangEditor.bar;
-
-		//编辑器改变事件时，同步更新文章内容
-		editor.onchange = function () {
-
-			var text = this.$txt.text().replace(/(^\s*)|(\s*$)/g, ""),
-				html = this.$txt.html();
-
-			/*console.log(text);
-			 console.log(html);*/
-
-			self.setContent(html, text);
-		};
-
-		// editor.config.hideLinkImg = true;
-
-		//自定义上传图片错误事件
-		editor.create();
+		this.initWangeditor();
 
 		if (this.$route.query.id) {
 			var data = {
@@ -171,8 +186,7 @@ export default {
 					this.article_data        = data.article_info;
 					this.article_data.status = data.article_info.status == 1 ? true : false;
 					this.article_data.tabs   = data.article_info.tabs.split(',');
-
-					$("#article").html(this.article_data.content);
+                    this.wangEditor.obj.$txt.html(this.article_data.content);
 				}
 			});
 		}
