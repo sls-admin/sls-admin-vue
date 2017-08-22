fe23beeba18703264af5337cb1a8942897044c4c
sailengsi <sailengsi@126.com>
2017-08-23 02:18:39 +0800

    test

--- src/layout/head-nav/HeadNav.vue
+++ src/layout/head-nav/HeadNav.vue
@@ -34,8 +34,7 @@
                                         command='pass'
                                         v-if='$store.state.user.userinfo.is_update_pass'>修改密码</el-dropdown-item>
                                 <el-dropdown-item
-                                        command='set'
-                                        v-if='$store.state.user.userinfo.pid==0'>系统设置</el-dropdown-item>
+                                        command='set'>系统设置</el-dropdown-item>
                                 <el-dropdown-item
                                         command='logout'>退出</el-dropdown-item>
                             </el-dropdown-menu>
@@ -105,28 +104,7 @@
                      label-width="100px"
                      v-model='dialog.set_info'
                      ref='set_info'>
-                <el-form-item label="登录方式">
-                    <el-select placeholder="请选择登录方式"
-                               v-model='dialog.set_info.login_style'>
-                        <el-option label="单一登录" value="1"></el-option>
-                        <el-option label="多点登录" value="2"></el-option>
-                    </el-select>
-                </el-form-item>
-                <el-form-item label="禁止修改密码">
-                    <el-select placeholder="请选择用户"
-                               multiple
-                               v-model='dialog.set_info.disabled_update_pass'>
-                        <!-- value的值的ID是number，disabled_update_pass的元素中的是字符串，
-                            所以在value上，需要拼装一个空串，来转化
-                            因为elementUI内部用了=== 
-                        -->
-                        <el-option
-                                v-for='(user,index) in dialog.set_info.select_users'
-                                :key='index'
-                                :label='user.username'
-                                :value='user.id+""'></el-option>
-                    </el-select>
-                </el-form-item>
+
             </el-form>
             <span slot="footer" class="dialog-footer">
                 <el-button @click="dialog.show_set = false">取 消</el-button>
@@ -138,60 +116,62 @@
 
 <script>
 	import HeadNavJs from './HeadNav.js';
+
 	export default HeadNavJs;
 </script>
 
 <style scoped lang='less'>
     .logo-container {
-        height: 60px;
+        height : 60px;
     }
 
     .logo {
-        height: 50px;
-        width: auto;
-        margin-left: 10px;
-        margin-top: 5px;
+        height      : 50px;
+        width       : auto;
+        margin-left : 10px;
+        margin-top  : 5px;
     }
 
     .fa-user {
-        position: relative;
-        top: -2px;
-        margin-right: 4px;
+        position     : relative;
+        top          : -2px;
+        margin-right : 4px;
     }
 
     .head-nav {
-        width: 100%;
-        height: 60px;
-        background: #324057;
-        position: fixed;
-        top: 0px;
-        left: 0px;
-        z-index: 99;
-        color: #FFF;
-        border-bottom: 1px solid #1F2D3D;
+        width         : 100%;
+        height        : 60px;
+        background    : #324057;
+        position      : fixed;
+        top           : 0px;
+        left          : 0px;
+        z-index       : 99;
+        color         : #FFF;
+        border-bottom : 1px solid #1F2D3D;
 
-    .logout {
-        width: 60px;
-        height: 60px;
-        line-height: 60px;
-        text-align: center;
-        float: right;
-        cursor: pointer;
+        .logout {
+            width       : 60px;
+            height      : 60px;
+            line-height : 60px;
+            text-align  : center;
+            float       : right;
+            cursor      : pointer;
+        }
+
     }
 
-    }
     .userinfo {
-        text-align: right;
+        text-align : right;
     }
 
     .username {
-        height: 60px;
-        line-height: 60px;
-        cursor: pointer;
+        height      : 60px;
+        line-height : 60px;
+        cursor      : pointer;
 
-    .el-dropdown {
-        color: #FFF;
-    }
+        .el-dropdown {
+            color : #FFF;
+        }
 
     }
 </style>
